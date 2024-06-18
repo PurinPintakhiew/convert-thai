02694ffe21f8d4a83a24c5d287c9e946ae7f0ab2
PurinPintakhiew <dev.purin@gmail.com>
2024-06-19 00:01:39 +0700

    feat : create index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import dateFormat from "./lib/DateConverter";
+import numberFormat from "./lib/NumberConverter";
+
+const convertThai = {
+  dateFormat,
+  numberFormat,
+};
+
+export default convertThai;
