72bd69ba4e119a9b4272051d01737787615e9a46
PurinPintakhiew <dev.purin@gmail.com>
2024-06-25 10:36:27 +0700

    chore : bathtext

--- dist/src/lib/BathTextConverter.js
+++ dist/src/lib/BathTextConverter.js
@@ -1,10 +1,24 @@
 "use strict";
 const bathConstants = {
-    numbers: [],
-    units: [],
+    numbers: [
+        "",
+        "หนึ่ง",
+        "สอง",
+        "สาม",
+        "สี่",
+        "ห้า",
+        "หก",
+        "เจ็ด",
+        "แปด",
+        "เก้า",
+    ],
+    units: ["", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน"],
 };
 const bathText = (number) => {
     try {
+        if (number === null || typeof number === "undefined" || isNaN(number)) {
+            return "";
+        }
         return "";
     }
     catch (error) {
--- src/lib/BathTextConverter.ts
+++ src/lib/BathTextConverter.ts
@@ -4,12 +4,27 @@ interface BathConstants {
 }
 
 const bathConstants: BathConstants = {
-  numbers: [],
-  units: [],
+  numbers: [
+    "",
+    "หนึ่ง",
+    "สอง",
+    "สาม",
+    "สี่",
+    "ห้า",
+    "หก",
+    "เจ็ด",
+    "แปด",
+    "เก้า",
+  ],
+  units: ["", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน"],
 };
 
 const bathText = (number: number): string => {
   try {
+    if (number === null || typeof number === "undefined" || isNaN(number)) {
+      return "";
+    }
+    
     return "";
   } catch (error) {
     console.error(error);
