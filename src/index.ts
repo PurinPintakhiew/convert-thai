ea3e1ff95ce42dee191db2903235e15d61fb5c64
PurinPintakhiew <dev.purin@gmail.com>
2024-06-26 11:36:51 +0700

    feat : add bathText

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { dateFormat } from "./lib/DateConverter";
 import { numberFormat } from "./lib/NumberConverter";
+import { bathText } from "./lib/BathTextConverter";
 
 const convertThai = {
   dateFormat,
   numberFormat,
+  bathText,
 };
 
-export { dateFormat, numberFormat, convertThai };
+export { dateFormat, numberFormat, bathText, convertThai };
