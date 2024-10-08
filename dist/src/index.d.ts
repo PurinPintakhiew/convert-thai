e828ec2a42b882cb27107f725ec8089ca904e49b
PurinPintakhiew <purin.pintakhiew@compattana.com>
2024-10-08 16:09:05 +0700

    build : rebuild

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,9 +1,9 @@
-import { dateFormat, Era } from "./lib/DateConverter";
+import { dateFormat, Era, Locale } from "./lib/DateConverter";
 import { numberFormat, ThousandsSeparator } from "./lib/NumberConverter";
 import { bathText } from "./lib/BathTextConverter";
 interface ConvertThai {
     bathText: (number: number) => string;
-    dateFormat: (date: Date | string, format?: string, era?: Era) => string;
+    dateFormat: (date: Date | string, format?: string, era?: Era, locale?: Locale) => string;
     numberFormat: (number: number, decimals?: number, thousandsSeparator?: ThousandsSeparator) => string;
 }
 declare const convertThai: ConvertThai;
--- dist/src/lib/DateConverter.d.ts
+++ dist/src/lib/DateConverter.d.ts
@@ -1,3 +1,4 @@
 type Era = "BE" | "CE" | "MS" | "JE" | "RE" | "HE";
-declare const dateFormat: (date: any, format?: string, era?: Era) => string;
-export { dateFormat, Era };
+type Locale = "th" | "us";
+declare const dateFormat: (date: any, format?: string, era?: Era, locale?: Locale) => string;
+export { dateFormat, Era, Locale };
